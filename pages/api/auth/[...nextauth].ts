5a42c6111432f37db1e5f3c6ea3a7211ed9c8107
ifan4 <ifana.andri@upi.edu>
2023-07-11 13:59:25 +0700

    update url public nxthauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -27,7 +27,7 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials, req) {
         const { email, password } = credentials as any;
-        const res = await fetch(`http://127.0.0.1:8000/api/authentication/login`, {
+        const res = await fetch(`https://fan-academia-q96sz.ondigitalocean.app/api/authentication/login`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
